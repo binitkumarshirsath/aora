4df5752d4fc50d0f73e076b856d9bbd6012db5dc
binitkumarshirsath <binitkumar0911@gmail.com>
2024-05-12 09:14:52 +0530

    initialized a expo router with nativewind

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const TabOne = () => {
+  return (
+    <View className="bg-red-500">
+      <Text>TabOne</Text>
+    </View>
+  );
+};
+
+export default TabOne;
